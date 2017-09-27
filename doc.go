822f13e1ff0c8cf19de5aca4378291cde7fa61cb
mikattack <alex@mikitik.com>
2017-09-27 16:12:13 -0700

    Update code documentation.

--- doc.go
+++ doc.go
@@ -8,15 +8,12 @@ The easiest way to use mlog is through the package-level logger:
   import log "github.com/mikattack/mlog"
 
   func main() {
-    log.WithFields({
-      "warehouse":  "phx-4",
-      "order":      51132000,
-    }).ToInvestigate("Negative inventory applied to order")
+    log.ToInvestigate("Non-existent inventory applied to order: 51138000")
   }
 
 Output:
 
-  time="2017-09-22T08:52:33Z" level=warning warehouse="phx-4" order=5113200 msg="Negative inventory applied to order"
+  [WARNING] 2017-09-22 08:52:33 orders.go:513 Non-existent inventory applied to order: 51138000
 
 The logging API is limited and intentionally simplistic. It eschews fine-grain
 levels for obvious ones:
